697acff03ee58165a85cdce14af4eb9ac003a66f
user <user@user-PC>
2020-09-18 22:13:17 +0530

    Added TestNG library to Cucumber

--- BDDCucumber/src/main/java/Runner/testRunner.java
+++ BDDCucumber/src/main/java/Runner/testRunner.java
@@ -4,14 +4,17 @@ package Runner;
 import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
 
 
-@RunWith(Cucumber.class)
+//@RunWith(Cucumber.class)
 @CucumberOptions(
 features = "G:\\Siddharth\\Workspace\\latest\\BDDCucumber\\Feature_Files\\test.feature"
-,glue={"StepDefinition"}
+,glue={"StepDefinition"},
+plugin= {"pretty"},
+monochrome=true
 )
 
-public class testRunner {
+public class testRunner extends AbstractTestNGCucumberTests {
 
 }
--- BDDCucumber/src/main/java/StepDefinition/StepDefinition.java
+++ BDDCucumber/src/main/java/StepDefinition/StepDefinition.java
@@ -1,10 +1,12 @@
 package StepDefinition;
 
-import org.junit.Before;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import cucumber.api.PendingException;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -12,43 +14,49 @@ import cucumber.api.java.en.When;
 public class StepDefinition {
 	
 	WebDriver driver ;
-	
-	@Given("^user has an url$")
-	public void user_has_an_url() throws Throwable {
-	
-		System.setProperty("webdriver.chrome.driver", "G:\\Siddharth\\Workspace\\latest\\BDDCucumber\\Drivers\\chromedriver.exe");
+
+	@Before
+	public void init() {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\user\\git\\BDD-Cucumber-project\\BDDCucumber\\Drivers\\chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.get("https://www.redbus.com/");
+		driver.get("https://www.redbus.in/");
+
+	}
+		
+	@Given("^user has an url$")
+	public void user_has_an_url() throws Throwable {
+	    System.out.println("url");
+	    throw new PendingException();
+	}
+	
+	@When("^user opens url$")
+	public void user_opens_url() throws Throwable {
+	    System.out.println("User is on mentioned website");
 	    throw new PendingException();
 	}
 
-//	@When("^user opens url$")
-//	public void user_opens_url() throws Throwable {
-//	    // Write code here that turns the phrase above into concrete actions
-//	    throw new PendingException();
-//	}
-//
-//	@Then("^user needs to enter userid and password$")
-//	public void user_needs_to_enter_userid_and_password() throws Throwable {
-//	    // Write code here that turns the phrase above into concrete actions
-//	    throw new PendingException();
-//	}
-//
-//	@Then("^click on submit button$")
-//	public void click_on_submit_button() throws Throwable {
-//	    // Write code here that turns the phrase above into concrete actions
-//	    throw new PendingException();
-//	}
-//
-//	@Then("^user is logged in successfully$")
-//	public void user_is_logged_in_successfully() throws Throwable {
-//	    // Write code here that turns the phrase above into concrete actions
-//	    throw new PendingException();
-//	}
-//
+	@Then("^user needs to enter userid and password$")
+	public void user_needs_to_enter_userid_and_password() throws Throwable {
+	    System.out.println("id");
+	    throw new PendingException();
+	}
 
-	@Before
+	@Then("^click on submit button$")
+	public void click_on_submit_button() throws Throwable {
+		System.out.println("submit");
+	    throw new PendingException();
+	}
+
+	@Then("^user is logged in successfully$")
+	public void user_is_logged_in_successfully() throws Throwable {
+	    System.out.println("User is logged in successfully");
+	    throw new PendingException();
+	}
+
+
+	@After
 	public void teardown() {
 		driver.quit();
 	}
