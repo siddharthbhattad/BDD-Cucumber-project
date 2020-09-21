ced35e79247a96d5c1f54f84677aaa67d0e95acf
user <user@user-PC>
2020-09-21 16:29:26 +0530

    deleted target files

--- BDDCucumber/src/main/java/Runner/testRunner.java
+++ BDDCucumber/src/main/java/Runner/testRunner.java
@@ -10,9 +10,9 @@ import cucumber.api.testng.AbstractTestNGCucumberTests;
 //@RunWith(Cucumber.class)
 @CucumberOptions(
 features = "G:\\Siddharth\\Workspace\\latest\\BDDCucumber\\Feature_Files\\test.feature"
-,glue={"StepDefinition"},
-plugin= {"pretty"},
-monochrome=true
+,glue={"StepDefinition"}
+//plugin= {"pretty"},
+//monochrome=true
 )
 
 public class testRunner extends AbstractTestNGCucumberTests {
